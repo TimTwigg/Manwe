753033153607be5b3863215fc8e49130cccb2df9
TimTwigg <tim@twiggusa.com>
2024-09-20 17:52:52 -0700

    [UDPATE] Add ReactionCount to stat block

--- types/stat_blocks/numerical_attributes.go
+++ types/stat_blocks/numerical_attributes.go
@@ -22,15 +22,16 @@ type HitPointsT struct {
 }
 
 type NumericalAttributes struct {
-	ArmorClass   int
-	HitPoints    HitPointsT
-	Speed        Speeds
-	Strength     int
-	Dexterity    int
-	Constitution int
-	Intelligence int
-	Wisdom       int
-	Charisma     int
+	ArmorClass    int
+	HitPoints     HitPointsT
+	Speed         Speeds
+	ReactionCount int
+	Strength      int
+	Dexterity     int
+	Constitution  int
+	Intelligence  int
+	Wisdom        int
+	Charisma      int
 }
 
 func (s Speeds) Dict() map[string]interface{} {
@@ -52,15 +53,16 @@ func (h HitPointsT) Dict() map[string]interface{} {
 
 func (n NumericalAttributes) Dict() map[string]interface{} {
 	return map[string]interface{}{
-		"ArmorClass":   n.ArmorClass,
-		"HitPoints":    n.HitPoints,
-		"Speed":        n.Speed,
-		"Strength":     n.Strength,
-		"Dexterity":    n.Dexterity,
-		"Constitution": n.Constitution,
-		"Intelligence": n.Intelligence,
-		"Wisdom":       n.Wisdom,
-		"Charisma":     n.Charisma,
+		"ArmorClass":    n.ArmorClass,
+		"HitPoints":     n.HitPoints,
+		"Speed":         n.Speed,
+		"ReactionCount": n.ReactionCount,
+		"Strength":      n.Strength,
+		"Dexterity":     n.Dexterity,
+		"Constitution":  n.Constitution,
+		"Intelligence":  n.Intelligence,
+		"Wisdom":        n.Wisdom,
+		"Charisma":      n.Charisma,
 	}
 }
 
@@ -103,21 +105,22 @@ func ParseNumericalAttributesData(dict map[string]interface{}) (parse.Parseable,
 		return NumericalAttributes{}, errors.ParseError{Message: fmt.Sprintf("HitPoints key missing from Numerical Attributes dictionary! (%v)", dict)}
 	}
 
-	missingKey := errors.ValidateKeyExistance(dict, []string{"ArmorClass", "HitPoints", "Speed", "Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"})
+	missingKey := errors.ValidateKeyExistance(dict, []string{"ArmorClass", "HitPoints", "Speed", "ReactionCount", "Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"})
 	if missingKey != nil {
 		return NumericalAttributes{}, errors.ParseError{Message: fmt.Sprintf("Key '%s' missing from Numerical Attributes dictionary! (%v)", *missingKey, dict)}
 	}
 
 	return NumericalAttributes{
-		ArmorClass:   int(dict["ArmorClass"].(float64)),
-		HitPoints:    hitPointsDict.(HitPointsT),
-		Speed:        speedDict.(Speeds),
-		Strength:     int(dict["Strength"].(float64)),
-		Dexterity:    int(dict["Dexterity"].(float64)),
-		Constitution: int(dict["Constitution"].(float64)),
-		Intelligence: int(dict["Intelligence"].(float64)),
-		Wisdom:       int(dict["Wisdom"].(float64)),
-		Charisma:     int(dict["Charisma"].(float64)),
+		ArmorClass:    int(dict["ArmorClass"].(float64)),
+		HitPoints:     hitPointsDict.(HitPointsT),
+		Speed:         speedDict.(Speeds),
+		ReactionCount: int(dict["ReactionCount"].(float64)),
+		Strength:      int(dict["Strength"].(float64)),
+		Dexterity:     int(dict["Dexterity"].(float64)),
+		Constitution:  int(dict["Constitution"].(float64)),
+		Intelligence:  int(dict["Intelligence"].(float64)),
+		Wisdom:        int(dict["Wisdom"].(float64)),
+		Charisma:      int(dict["Charisma"].(float64)),
 	}, nil
 }
 
